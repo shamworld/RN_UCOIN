896c16f739b566a486ed0eeecddb0e0c3b3203be
ios <75328092@qq.com>
2018-04-12 18:08:29 +0800

    no message

--- src/Login/EmailTextView.js
+++ src/Login/EmailTextView.js
@@ -14,18 +14,31 @@ import Msg from '../../Compent/LoadingMsg';
 
 const {width,height}=Dimensions.get('window');
 export default class EmailTextView extends Component{
+    constructor(props){
+        super(props);
+    }
 
     static navigationOptions = {
-        title:'邮箱验证',
-        cardStack:{
-            gesturesEnabled:false,
-        },
+        header:null,
+    }
+    
+    emailCodeClick(){
+        alert('邮箱验证码');
     }
 
     render(){
+
+        var emailStr=this.props.navigation.state.params.email;
         return(
             <View style={stypes.contian}>
-                <Text stype={{color:'#fff'}}>{this.props.email}</Text>
+                <Text style={stypes.headerText}>邮箱验证</Text>
+                <Image source={require('../../Images/图标/mailverify.png')} style={stypes.imageIcon} resizeMode='center'/>
+                <Text style = {stypes.validationText}>验证您的邮箱</Text>
+                <Text style={stypes.contentText} numberOfLines = {2}>激活邮件已发送至{emailStr},请登录您的邮箱进行激活,该链接15分钟内有效</Text>
+                <TouchableOpacity activeOpacity = {1} onPress = {() => this.emailCodeClick()}>
+                    <Text style={stypes.emailCodeText}>邮箱验证码</Text>
+                </TouchableOpacity>
+                <View style={{height:1,marginLeft:50,marginRight:50,backgroundColor:'#fff'}}></View>
             </View>
         )
     }
@@ -40,5 +53,47 @@ const stypes=StyleSheet.create({
         height:height,
         backgroundColor:'rgb(27,39,45)',
     },
-    
+    headerText:{
+        marginTop:20,
+        height:44,
+        lineHeight:44,
+        textAlign:'center',
+        width:width,
+        color:'rgb(196,199,201)'
+    },
+    imageIcon:{
+        marginTop:64,
+        marginLeft:width/2.0-45,
+        width:90,
+        height:75,
+    },
+    validationText:{
+        marginTop:5,
+        marginLeft:0,
+        marginRight:0,
+        textAlign:'center',
+        height:20,
+        lineHeight:20,
+        color:'#fff',
+        fontSize:14
+    },
+    contentText:{
+        marginTop:40,
+        marginLeft:25,
+        marginRight:25,
+        textAlign:'center',
+        color:'#fff',
+        fontSize:14,
+    },
+    emailCodeText:{
+        marginTop:50,
+        marginLeft:50,
+        marginRight:50,
+        height:35,
+        borderBottomWidth:1,
+        borderBottomColor:'#fff',
+        lineHeight:35,
+        textAlign:'center',
+        color:'#fff'
+    }
 });
